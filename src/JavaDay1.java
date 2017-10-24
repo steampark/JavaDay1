3ed0f49513ff969290a7cff0ea0e5156d64aac97
user <user@com03>
2017-10-24 20:59:01 +0900

    Just Changed

--- src/JavaDay1.java
+++ src/JavaDay1.java
@@ -4,6 +4,10 @@ public class JavaDay1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+
 	}
 
 }
